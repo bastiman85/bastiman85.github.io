57d6782485b506269256451f89e5b2bbed5dd6f7
bastiman85 <github@bastiman.nu>
2021-01-16 17:16:41 +0100

    Minor fixes

--- loopstudios-landing-page/js/main.js
+++ loopstudios-landing-page/js/main.js
@@ -4,8 +4,15 @@ const nav = document.querySelector(".hero nav");
 
 hamburger.addEventListener("click", () => {
    nav.classList.add("nav--visible");
+   nav.classList.remove("nav--hidden");
+   nav.classList.remove("nav");
 });
 
 close.addEventListener("click", () => {
    nav.classList.remove("nav--visible");
+
+   nav.classList.add("nav--hidden");
+   setTimeout(function () {
+      nav.classList.add("nav");
+   }, 250);
 });
